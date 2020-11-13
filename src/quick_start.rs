81aabb3e59bf9937c2e7d01fd322c0597ce3e835
Luis Moreno <luis.moreno@edunext.co>
2020-11-12 23:47:45 -0400

    Add example for GaussianNB

--- src/quick_start.rs
+++ src/quick_start.rs
@@ -8,6 +8,8 @@ use smartcore::math::distance::Distances;
 use smartcore::neighbors::knn_classifier::*;
 // Imports for Logistic Regression
 use smartcore::linear::logistic_regression::LogisticRegression;
+// Imports Gaussian Naive Bayes classifier
+use smartcore::naive_bayes::GaussianNB;
 // Model performance
 use smartcore::metrics::accuracy;
 
@@ -77,4 +79,26 @@ pub fn iris_lr_ndarray_example() {
 
     // Calculate training error
     println!("accuracy: {}", accuracy(&y, &y_hat)); // Prints 0.98
+}
+
+pub fn iris_gaussiannb_example() {
+    // Load Iris dataset
+    let iris_data = load_dataset();
+
+    // Turn Iris dataset into NxM matrix
+    let x = DenseMatrix::from_array(
+        iris_data.num_samples,
+        iris_data.num_features,
+        &iris_data.data,
+    );
+
+    // These are our target class labels
+    let y = iris_data.target;
+
+    // Fit Logistic Regression to Iris dataset
+    let gnb = GaussianNB::fit(&x, &y, Default::default()).unwrap();
+    let y_hat = gnb.predict(&x).unwrap(); // Predict class labels
+
+    // Calculate training error
+    println!("accuracy: {}", accuracy(&y, &y_hat)); // Prints 0.96
 }
